882a3367b6c2514a462f7f03b4ae45e8f6334a43
UsielOM <usiel31197@outlook.es>
2022-08-08 20:05:07 -0500

    Cambiando unos titulos

--- app/DataBase/mariadb.js
+++ app/DataBase/mariadb.js
@@ -3,7 +3,7 @@ const sequelize = require('./conexionBD');
 init = function() {
 
     sequelize.authenticate().then(() => {
-        console.log("Conexion establecida exitosamente con mysql.");
+        console.log("Conexion establecida exitosamente con mariadb.");
 
     }).catch(err => {
         console.error("Conexion no estableccida:", err);
