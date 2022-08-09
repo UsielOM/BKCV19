f6204525dba5411988b39a977cf04f991a7f9512
UsielOM <usiel31197@outlook.es>
2022-08-09 16:53:25 -0500

    Modelo Usuarios

---
+++ app/DataBase/Tablas/Usuarios.js
@@ -0,0 +1,36 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../conexionBD');
+
+const Usuarios = sequelize.define('usuarios', {
+    idUsuario: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    }
+
+}, {
+    freezeTableName: true, //Esta linea va en todos los modelos que hagan para que respete el nombre
+    timestamps: false, //Esta linea va en todos los modelos para que no les cree los campo de Creacion y actualizacion 
+
+})
+module.exports = Usuarios
