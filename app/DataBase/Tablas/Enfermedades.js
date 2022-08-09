3f5ad4d728b2024bee6f2ec1a6f7bdf265fcd0a5
UsielOM <usiel31197@outlook.es>
2022-08-09 17:05:15 -0500

    Modelo enfermedades

---
+++ app/DataBase/Tablas/Enfermedades.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../conexionBD');
+
+const Enfermedades = sequelize.define('enfermedades', {
+
+        idIllness: {
+            type: Sequelize.DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Illness: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        freezeTableName: true, //Esta linea va en todos los modelos que hagan para que respete el nombre
+        timestamps: false, //Esta linea va en todos los modelos para que no les cree los campo de Creacion y actualizacion 
+
+    }
+
+)
