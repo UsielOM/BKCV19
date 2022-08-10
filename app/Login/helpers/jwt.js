4a8369a053d82c691087f1c29c2008f4a5c02d63
UsielOM <usiel31197@outlook.es>
2022-08-10 16:42:41 -0500

    Jwt archivo

---
+++ app/Login/helpers/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (name, email) => {
+    const playload = { name, email };
+    return new Promise((resolve, reject) => {
+        jwt.sign(playload, process.env.SECRET_JWT_SEED,
+
+            {
+                expiresIn: '4h'
+            },
+            (err, token) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(token)
+                }
+            })
+    });
+}
+
+module.exports = {
+    generarJWT
+}
