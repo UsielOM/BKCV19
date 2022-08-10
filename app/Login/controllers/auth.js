2b770398e443bfe95a30fb94172dac07c2cc1764
UsielOM <usiel31197@outlook.es>
2022-08-10 17:18:02 -0500

    Cambair y encryptar la contraseña

--- app/Login/controllers/auth.js
+++ app/Login/controllers/auth.js
@@ -47,7 +47,59 @@ const loginUsuario = async(req, res) => {
     }
 }
 
+const cambiarPassword = async(req, res = response) => {
+    const { email, password } = req.body;
+    let pw2;
+
+    try {
+        //validar usuario
+        const dbUser = await sequelize.query(
+            "Select a.email, a.password,  a.name from usuarios a  where a.email = '" + email + "'; ", {
+                type: sequelize.QueryTypes.SELECT
+            });
+        if (dbUser >= 0) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo no existe',
+                user: dbUser
+            })
+        }
+        //hashear la contraseña
+        const salt = bcrypt.genSaltSync();
+        pw2 = bcrypt.hashSync(password, salt);
+
+        //crear JWT
+        const token = await generarJWT(dbUser[0].name, email);
+
+        //Generar el update a la tabla
+        Usuarios.update({
+            password: pw2
+        }, {
+            where: {
+                idUsuario: dbUser[0].idUsuario
+            }
+        }).then(result => {
+            console.log(result)
+        })
+
+        return res.status(201).json({
+            ok: true,
+            msg: 'Se cambio la contraseña',
+            dbUser,
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+}
+
 
 module.exports = {
-    loginUsuario
+    loginUsuario,
+    cambiarPassword
 }
